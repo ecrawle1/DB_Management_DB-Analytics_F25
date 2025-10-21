8889c637871b703997e8925a09fa68374e6be952
Elizabeth Crawley <ecrawle1@kent.edu>
2025-10-21 09:24:05 -0400

    Fix foreign key constraints and typos in SQL script

--- SQL_DDL_DML_Practice_Set.sql
+++ SQL_DDL_DML_Practice_Set.sql
@@ -31,7 +31,7 @@ create table Offspring (
     regNum VARCHAR2(3),
     parent VARCHAR2(3),
     CONSTRAINT Offspring_regNum_parent_pk PRIMARY KEY (regNum, parent),
-    CONSTRAINT Offspring_regNum_fk FOREIGN KEY (regNum) REFERENCES RaceHorse (regNum) on  DELETE SET NULL,
+    CONSTRAINT Offspring_regNum_fk FOREIGN KEY (regNum) REFERENCES RaceHorse (regNum) on DELETE SET NULL,
     CONSTRAINT Offspring_parent_fk FOREIGN KEY (parent) REFERENCES RaceHorse (regNum) on DELETE SET NULL
 );
 
@@ -39,23 +39,23 @@ create table OwnedBy (
     regNum VARCHAR2(3),
     pId VARCHAR2(3),
     ownerPercentage NUMBER(3),
-    CONSTRAINT OwnedBy_regNum_pId_pk PRIMARY KEY (regNum, pId)
-    CONSTRAINT OwnedBy_regNum_fk FOREIGN KEY (regNum) REFERENCES RaceHorse (regNum) on DELEETE CASCADE,
-    CONSTRAINT OwnedBy_pId_fk FOREIGN KEY (pId) REFERENCES Person (pId) on DELEETE CASCADE
+    CONSTRAINT OwnedBy_regNum_pId_pk PRIMARY KEY (regNum, pId),
+    CONSTRAINT OwnedBy_regNum_fk FOREIGN KEY (regNum) REFERENCES RaceHorse (regNum) on DELETE CASCADE,
+    CONSTRAINT OwnedBy_pId_fk FOREIGN KEY (pId) REFERENCES Person (pId) on DELETE CASCADE
 );
 
 create table RaceSchedule (
     schId VARCHAR2(3) PRIMARY KEY,
     schYear VARCHAR2(4),
     schMonth VARCHAR2(2),
-    schDay VARCHAR2(2),
+    schDay VARCHAR2(2)
 );
 
 create table Race (
     schId VARCHAR2(3),
     rNumber NUMBER(4),
     purse NUMBER(10),
-    CONSTRAINT Race_schId_rNumber_pk PRIMARY KEY (schId, rNumber)
+    CONSTRAINT Race_schId_rNumber_pk PRIMARY KEY (schId, rNumber),
     CONSTRAINT Race_schId_fk FOREIGN KEY (schId) REFERENCES RaceSchedule (schId) on DELETE CASCADE
 );
 
@@ -67,7 +67,7 @@ create table Entry (
     jockey VARCHAR2(3),
     horse VARCHAR2(3),
     CONSTRAINT Entry_schId_rNumber_gate_pk PRIMARY KEY (schId, rNumber, gate),
-    CONSTRAINT Entry_schId_rNumber_fk FOREIGN KEY (schId, rNumber) REFERENCES Race (schId, rNumber) on DELEETE CASCADE,
-    CONSTRAINT Entry_jockey_fk FOREIGN KEY (jockey) REFERENCES Person (pId) on DELEETE SET NULL,
-    CONSTRAINT Entry_horse_fk FOREIGN KEY (horse) REFERENCES RaceHorse (regNum) on DELEETE CASCADE
+    CONSTRAINT Entry_schId_rNumber_fk FOREIGN KEY (schId, rNumber) REFERENCES Race (schId, rNumber) on DELETE CASCADE,
+    CONSTRAINT Entry_jockey_fk FOREIGN KEY (jockey) REFERENCES Person (pId) on DELETE SET NULL,
+    CONSTRAINT Entry_horse_fk FOREIGN KEY (horse) REFERENCES RaceHorse (regNum) on DELETE CASCADE
 );
